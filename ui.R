78988884ec450b76312c5f9b2e86d0418f6472c2
Gordion <gordon.bryden@gmail.com>
2016-10-25 11:02:24 +0100

    Add formatting

--- ui.R
+++ ui.R
@@ -1,1 +1,11 @@
-library(shiny)# Define UI for application that draws a histogramfluidPage(  # Application title  titlePanel("Unique Baby Names in Scotland"),  # Sidebar with a slider input for the number of bins      # Show a plot of the generated distribution  mainPanel(    plotOutput("distPlot")  ))
+library(shiny)
+# Define UI for application that draws a histogram
+fluidPage(  
+  # Application title  
+  titlePanel("Unique Baby Names in Scotland"),  
+  # Sidebar with a slider input for the number of bins      
+  # Show a plot of the generated distribution
+  mainPanel(
+    plotOutput("distPlot")
+  )
+)
