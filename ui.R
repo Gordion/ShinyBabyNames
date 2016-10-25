c09c0070a89fe71f2ba42b58b8749c0b92d1b570
Gordion <gordon.bryden@gmail.com>
2016-10-25 10:41:06 +0100

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,1 @@
+library(shiny)# Define UI for application that draws a histogramfluidPage(  # Application title  titlePanel("Unique Baby Names in Scotland"),  # Sidebar with a slider input for the number of bins      # Show a plot of the generated distribution  mainPanel(    plotOutput("distPlot")  ))
